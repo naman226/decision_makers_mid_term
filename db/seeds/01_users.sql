ccbed34427274ba040e97a57c8add035422b5cc4
Kenny Moon <kennymoon90@gmail.com>
2022-07-02 17:35:06 -0700

    Add seed data for polls and choices tables, delete example files

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,213 @@
 -- Users table seeds here (Example)
 INSERT INTO users (name) VALUES ('Alice');
 INSERT INTO users (name) VALUES ('Kira');
+
+INSERT INTO polls (name, email)
+VALUES ('What activity should we do?', hiker@gmail.com);
+INSERT INTO polls (name, email)
+VALUES ("Where should we go on Vacation?", vacationer@gmail.com);
+INSERT INTO polls (name, email)
+VALUES ('What music should we listen to?', dj@gmail.com);
+INSERT INTO polls (name, email)
+VALUES('Where type of movie should we go see?', moviejunkie@gmail.com);
+
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Hiking', `We'll go hike the Grouse Grind`, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Beach day', `We'll go to Kits Beach and spend the day lying in the sun`, 4)
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Skiing', `We'll go up to Whistler and spend the day skiing, but mostly waiting in chairlift lines`, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Brewery Tour', `We'll walk around East Vancouver and visit as many craft breweries as we can`, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Hiking', `We'll go hike the Grouse Grind`, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Beach day', `We'll go to Kits Beach and spend the day lying in the sun`, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Skiing', `We'll go up to Whistler and spend the day skiing, but mostly waiting in chairlift lines`, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Brewery Tour', `We'll walk around East Vancouver and visit as many craft breweries as we can`, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Hiking', `We'll go hike the Grouse Grind`, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Beach day', `We'll go to Kits Beach and spend the day lying in the sun`, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Skiing', `We'll go up to Whistler and spend the day skiing, but mostly waiting in chairlift lines`, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Brewery Tour', `We'll walk around East Vancouver and visit as many craft breweries as we can`, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Hiking', `We'll go hike the Grouse Grind`, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Beach day', `We'll go to Kits Beach and spend the day lying in the sun`, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Skiing', `We'll go up to Whistler and spend the day skiing, but mostly waiting in chairlift lines`, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Brewery Tour', `We'll walk around East Vancouver and visit as many craft breweries as we can`, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Hiking', `We'll go hike the Grouse Grind`, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Beach day', `We'll go to Kits Beach and spend the day lying in the sun`, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Skiing', `We'll go up to Whistler and spend the day skiing, but mostly waiting in chairlift lines`, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Brewery Tour', `We'll walk around East Vancouver and visit as many craft breweries as we can`, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Hiking', `We'll go hike the Grouse Grind`, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Beach day', `We'll go to Kits Beach and spend the day lying in the sun`, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Skiing', `We'll go up to Whistler and spend the day skiing, but mostly waiting in chairlift lines`, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (1, 'Brewery Tour', `We'll walk around East Vancouver and visit as many craft breweries as we can`, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (2, 'Mexico', `All-inclusive Resort Vacation`, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (2, 'Alaskan Cruise', `All-inclusive Alaskan Cruise with stops along the Alaskan Coast`, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (2, 'France', `A 7 night trip starting in Paris and travelling to the south of France`, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (2, 'Tofino', `10 days stay in Tofino, relaxing, surfing and escaping the day to day grind`, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (2, 'Mexico', `All-inclusive Resort Vacation`, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (2, 'Alaskan Cruise', `All-inclusive Alaskan Cruise with stops along the Alaskan Coast`, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (2, 'France', `A 7 night trip starting in Paris and travelling to the south of France`, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (2, 'Tofino', `10 days stay in Tofino, relaxing, surfing and escaping the day to day grind`, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES(2, 'Mexico', `All-inclusive Resort Vacation`, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (2, 'Alaskan Cruise', `All-inclusive Alaskan Cruise with stops along the Alaskan Coast`, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (2, 'France', `A 7 night trip starting in Paris and travelling to the south of France`, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (2, 'Tofino', `10 days stay in Tofino, relaxing, surfing and escaping the day to day grind`, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (2, 'Mexico', `All-inclusive Resort Vacation`, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (2, 'Alaskan Cruise', `All-inclusive Alaskan Cruise with stops along the Alaskan Coast`, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (2, 'France', `A 7 night trip starting in Paris and travelling to the south of France`, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (2, 'Tofino', `10 days stay in Tofino, relaxing, surfing and escaping the day to day grind`, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'R & B', ``, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Top-40', ``, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Country', ``, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Rock', ``, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Top-40', ``, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'R & B', ``, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Country', ``, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Rock', ``, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Top-40', ``, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'R & B', ``, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Country', ``, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Rock', ``, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Top-40', ``, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'R & B', ``, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Country', ``, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Rock', ``, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Top-40', ``, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'R & B', ``, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Country', ``, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Rock', ``, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Top-40', ``, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'R & B', ``, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Country', ``, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Rock', ``, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Top-40', ``, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'R & B', ``, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Country', ``, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Rock', ``, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Top-40', ``, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'R & B', ``, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Country', ``, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Rock', ``, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Top-40', ``, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'R & B', ``, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Country', ``, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (3, 'Rock', ``, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Comedy', ``, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Drama', ``, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Action', ``, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Not Applicable', ``, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Comedy', ``, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Drama', ``, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Action', ``, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Not Applicable', ``, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Comedy', ``, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Drama', ``, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Action', ``, 1)
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Not Applicable', ``, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Comedy', ``, 2)
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Drama', ``, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Action', ``, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Not Applicable', ``, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Comedy', ``, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Drama', ``, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES(4, 'Action', ``, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Not Applicable', ``, 4);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Comedy', ``, 3);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Drama', ``, 2);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Action', ``, 1);
+INSERT INTO choices (poll_id, name, description, result_count)
+VALUES (4, 'Not Applicable', ``, 4);
