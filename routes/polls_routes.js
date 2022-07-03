8fa17ad4116569e6669570d0726833775b7a45ac
Naman Dhalla <namandhalla2261994@gmail.com>
2022-07-03 20:33:57 +0000

    Add impovement to the server

--- routes/polls_routes.js
+++ routes/polls_routes.js
@@ -1,13 +1,19 @@
-const express = ('express');
+const express = require('express');
+const dbParams = require('../lib/db');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('/(homepage.ejs)');
 });
 
+
 router.get('/:id', (req, res) => {
+  dbParams.query
   res.render('/(thank you for creating the poll.ejs)');
 })
+
+
 
 router.post('/', (req, res) => {
   res.redirect('/polls/:id');
--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const pollsRouter = require('./routes/polls_routes.js')
-const choicesRouter = require('./routes/choices_routes.js')
-const resultsRouter = require('./routes/results_routes.js')
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -21,9 +18,6 @@ db.connect();
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
-app.use('/polls', pollsRouter);
-app.use('/choices', choicesRouter);
-app.use('/results', resultsRouter);
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
@@ -42,11 +36,18 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const pollsRouter = require('./routes/polls_routes.js');
+const choicesRouter = require('./routes/choices_routes.js');
+const resultsRouter = require('./routes/results_routes.js');
+
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use('/polls', pollsRouter);
+app.use('/choices', choicesRouter);
+app.use('/results', resultsRouter);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
