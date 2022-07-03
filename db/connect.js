e2b961893eff93a11eba997dd8915415839860eb
Kenny Moon <kennymoon90@gmail.com>
2022-07-03 13:27:53 -0700

    Delete the separated db connect

--- db/connect.js
+++
@@ -1,8 +0,0 @@
-
-// PG database client/connection setup
-const { Pool } = require("pg");
-const dbParams = require("../lib/db.js");
-const db = new Pool(dbParams);
-db.connect();
-
-module.exports = db;
