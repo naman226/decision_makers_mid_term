a46c6234c6aa1f504f8e3c982e3712f100b3ddae
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-07-05 02:57:27 +0000

    implement route to choices with query data

--- routes/choices_routes.js
+++ routes/choices_routes.js
@@ -1,24 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
-  router.get('/', (req, res) => {
-    db.query(`SELECT * FROM choices QUERY;`)
-      .then(data => {
-        const templateVars = data.rows;
-        res.render('choices', templateVars);
+  router.get("/", (req, res) => {
+    db.query(
+    `SELECT polls.name as poll_name, choices.name, choices.description
+    FROM choices
+    JOIN polls ON poll_id = polls.id
+    WHERE poll_id = 1
+    GROUP BY polls.name, choices.name, choices.description
+    ORDER BY choices.name;`
+    )
+      .then((data) => {
+        const templateVars = {
+          choices: data.rows,
+          poll_name: data.rows[0].poll_name,
+        };
+        res.render("choices", templateVars);
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(404).send(err.message);
       });
   });
 
-  router.post('/:id', (req, res) => {
+  router.post("/:id", (req, res) => {
     db.query(`UPDATE choices count QUERY`)
       .then(() => {
-        res.redirect('results');
+        res.redirect("results");
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(404).send(err.message);
       });
   });
