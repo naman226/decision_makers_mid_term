ea69aa5b1fb7863f08d471e9bfa91f73cc2ca588
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-07-05 02:55:31 +0000

    implement route to /polls with query data

--- routes/polls_routes.js
+++ routes/polls_routes.js
@@ -5,24 +5,34 @@ const router = express.Router();
 module.exports = (db) => {
 
   router.get('/', (req, res) => {
+
     res.render('index');
     });
 
-
-  router.get('/:id', (req, res) => {
-    res.render('created');
-  });
-
   router.post('/', (req, res) => {
+    const email = req.body.email;
+    const name = req.body.name;
+    const option1 = req.body.option1;
+    const option2 = req.body.option2;
+    const option3 = req.body.option3;
+    const option4 = req.body.option4;
+    const description1 = req.body.description1;
+    const description2 = req.body.description2;
+    const description3 = req.body.description3;
+    const description4 = req.body.description4
+
     db.query(`INSERT INTO polls (name, email)
-    Values ('WHat to eat', 'xzy@gmail.com') RETURNING *;`)
+    Values ($1, $2) RETURNING *;`, [name, email])
       .then((polls) => {
         const pollId = polls.rows[0].id;
-        console.log(pollId);
+        console.log('pollId:', pollId);
         db.query(`INSERT INTO choices (poll_id, name, description)
-        VALUES (${pollId}, 'lets go kfc', 'fried chicken') RETURNING *;`)
+        VALUES (${pollId}, $1, $5),
+        (${pollId}, $2, $6),
+        (${pollId}, $3, $7),
+        (${pollId}, $4, $8) RETURNING *;`, [option1, option2, option3, option4, description1, description2, description3, description4])
         .then((choices) => {
-          console.log(choices.rows);
+          console.log('new data:', choices.rows);
           res.redirect(`/polls/${pollId}`);
         });
       })
@@ -30,7 +40,13 @@ module.exports = (db) => {
         res.status(404).send(err.message);
       });
   });
+
+  router.get('/:id', (req, res) => {
+
+    //url(req.params)
+    res.render('created');
+  });
+
   return router;
 };
-
 
