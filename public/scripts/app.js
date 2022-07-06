26cc3159734cba63191d79ad5b128d0ec7a62bfb
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-07-06 02:15:15 +0000

    add /choices/id route

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,40 @@
 // Client facing scripts here
+
+// $( document ).ready(function() {
+//   console.log('ready');
+//   const dragArea = $(".choices")
+//   // new Sortable(dragArea, {
+//   //   animation:350
+//   // })
+
+//   const children = dragArea.children()
+//   console.log(dragArea);
+//   console.log(children);
+
+
+// });
+$(document).ready(function() {
+  $(function(){ //slect by class
+    var $select = $(".1-4");
+    for (i=1;i<=4;i++){ //whenever it is less then 4 add 1
+        $select.append($('<option></option>').val(i).html(i)) //add it to the html tag options(to increase how many options)
+    }
+    // $("form").submit((event) => {
+    //   event.preventDefault();
+    //   console.log(event.target);
+    //   let vote = $('#vote').val();
+    //   console.log("whats the vote", vote);
+    // });
+
+  });
+
+  // $.ajax({
+  //   data: data,
+  //   success: success,
+  //   dataType: dataType
+  // });
+
+})
+
+
+
--- routes/choices_routes.js
+++ routes/choices_routes.js
@@ -2,14 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
-
   // Route to choices page where voters make their rankings of the choices in a poll
-  router.get("/", (req, res) => {
+  router.get("/:id", (req, res) => {
     db.query(
-    `SELECT polls.name as poll_name, choices.name, choices.description
+      `SELECT polls.name as poll_name, choices.name, choices.description
     FROM choices
     JOIN polls ON poll_id = polls.id
-    WHERE poll_id = 1
+    WHERE poll_id = ${req.params.id}
     GROUP BY polls.name, choices.name, choices.description
     ORDER BY choices.name;`
     )
@@ -27,13 +26,26 @@ module.exports = (db) => {
 
   // Route to submit the rankings of the choices in the poll => redirects to the results page
   router.post("/:id", (req, res) => {
-    db.query(`UPDATE choices count QUERY`)
-      .then(() => {
-        res.redirect("results");
-      })
-      .catch((err) => {
-        res.status(404).send(err.message);
-      });
+    const parsed = Object.entries(req.body);
+    console.log("req.body", parsed);
+    for (const element of parsed) {
+      const choice = element[0];
+      const vote = element[1];
+      console.log(element);
+      db.query(
+        `UPDATE choices  SET result_count = result_count + $1
+      WHERE choices.name = $2 AND poll_id = $3 RETURNING *;`,
+        [vote, choice, req.params.id]
+      )
+        .then((data) => {
+          res.status(200).send("Thanks for voting");
+          console.log(data.rows);
+        })
+        .catch((err) => {
+          res.status(404).send(err.message);
+        });
+    }
+    res.redirect("/results");
   });
   return router;
 };
